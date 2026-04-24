8af0feae451831b33a2526084f7cefab02c79858
aliter230880 <56067954+aliter230880@users.noreply.github.com>
2026-04-24 13:00:11 +0300

    Upload game files

---
+++ Assets/Scripts/UI/TabButton.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    public TabGroup tabGroup;
+    [Space]
+    public GameObject window;
+
+    public UnityEvent onTabSelected;
+    public UnityEvent onTabDeselected;
+
+    private Image bg;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.OnTabExit(this);
+    }
+
+    private void Awake()
+    {
+        bg = GetComponent<Image>();
+        tabGroup.Subscribe(this);
+    }
+
+
+    public void ChangeStyle(TabGroup.StateStyle style)
+    {
+        bg.color = style.color;
+        bg.sprite = style.sprite;
+    }
+
+    public void Select()
+    {
+        onTabSelected?.Invoke();
+        if(window != null)
+            window.SetActive(true);
+    }
+
+    public void Deselect()
+    {
+        onTabDeselected?.Invoke();
+        if(window != null)
+            window.SetActive(false);
+    }
+}
